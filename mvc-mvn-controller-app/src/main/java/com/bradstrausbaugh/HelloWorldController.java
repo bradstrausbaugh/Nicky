06b2983615b4e19b11f8e07911126ab19204c2b1
bradstrausbaugh <bradstraus@hotmail.com>
2020-08-30 12:13:19 -0400

    Resolved missing staging file during last commit

---
+++ mvc-mvn-controller-app/src/main/java/com/bradstrausbaugh/HelloWorldController.java
@@ -0,0 +1,33 @@
+package com.bradstrausbaugh;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		String theName = request.getParameter("studentName");
+		
+		theName = "Yo! " + theName.toUpperCase();
+		model.addAttribute("message", theName);
+		
+		return "helloworld";
+	}
+
+}
