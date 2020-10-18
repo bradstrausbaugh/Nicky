9a5e1d0de59c7172e6aa4130add113c95c58b8a5
brads <brads@DESKTOP-3DQKL7Q>
2020-10-18 10:54:50 -0400

    Added ReadStudentDemo.java for some more queries.

--- hibernate-tutorial/src/main/java/com/luv2code/hibernate/demp/ReadStudentDemo.java
+++ hibernate-tutorial/src/main/java/com/luv2code/hibernate/demp/ReadStudentDemo.java
@@ -1,5 +1,7 @@
 package com.luv2code.hibernate.demp;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,42 +22,54 @@ public class ReadStudentDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
-			// use the session object to save a new Student object
 			
-			// first, instantiate the POJO - Java 101
-			System.out.println("Creating a new Student object.");
-			Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+			// use the session object to save a new Student object
 			
 			// then, start a transaction and save the new Student instance
 			session.beginTransaction();
-			System.out.println("Saving the student...");
-			System.out.println(tempStudent);  // note: Id wb 0 at this point
-			session.save(tempStudent);
+			
+			// query the students
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			
+			// display the students
+			displayStudents(theStudents);
+			
+			// query all student whose lastName is "Doe"...
+			System.out.println("\n\nDisplay Student with lastName of 'Doe");
+			theStudents = session.createQuery("from Student s where s.lastName = 'Doe'").getResultList();
+				// note: we use s.lastName, the property not the getter
+			displayStudents(theStudents);
+			
+			// query all student whose lastName is "Doe" and firstName is "Daffy" ...
+			System.out.println("\n\nDisplay Students with lastName of 'Doe' and firstName is 'Daffy'...");
+			theStudents = session
+					.createQuery("from Student s where s.lastName = 'Doe' or s.firstName = 'Daffy'")
+					.getResultList();
+				// note: we use properties not getters
+			displayStudents(theStudents);
+			
+			// query all student whose email ends with 'luv2code.com'...
+			System.out.println("\n\nDisplay Students whose email ends with 'luv2code.com'");
+			theStudents = session.createQuery("from Student s where s.email LIKE '%luv2code.com'").getResultList();
+				// note: we use properties not getters
+			displayStudents(theStudents);
+
+			// commit transaction
 			session.getTransaction().commit(); // don't forget to commit
 			
-			// find out the student's id (primary key) - no longer null, having been saved
-			System.out.println("Saved Student - generated Id: " + tempStudent.getId());
-			
-			// get a new session and start a transaction
-			// - you always use transactions in Hibernate, even for reads
-			session = factory.getCurrentSession();
-			session.beginTransaction();
-			
-			// retrieve student based on the Id: primary key
-			System.out.println("\nGetting student with Id: " + tempStudent.getId());
-			Student myStudent = session.get(Student.class, tempStudent.getId());
-				// myStudent wb null if this Id was not in the table
-			System.out.println("myStudent: " + myStudent);
-			
-			// commit the transaction
-			session.getTransaction().commit();
-			
 			System.out.println("Done!");
+			
 		}
 		finally {
 			factory.close();
 		}
 
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 }
