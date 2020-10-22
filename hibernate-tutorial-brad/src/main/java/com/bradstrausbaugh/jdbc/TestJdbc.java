29bfcd5675ed5acd0f85af66478ed96669362240
brad <brad@brad-Inspiron-5570>
2020-10-22 17:54:42 -0400

    Initial commit for hibernate-tutorial-brad

---
+++ hibernate-tutorial-brad/src/main/java/com/bradstrausbaugh/jdbc/TestJdbc.java
@@ -0,0 +1,29 @@
+package com.bradstrausbaugh.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = 
+				"jdbc:mysql://localhost:3306/hb_employee_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "hbemployee";
+		String pass = "hbemployee";
+		
+		try {
+			System.out.println("Connecting to database " + jdbcUrl);
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!");
+			
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
