75888ff303514cb68c3d275c5a63d09baab44da0
brad <brad@brad-Satellite-P50-A>
2020-11-10 12:27:10 -0500

    Just changed @RequestMapping to @GetMapping in CustomerController.java

--- web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
@@ -18,7 +19,8 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	// @RequestMapping("/list") // use this for all HTTP methods - like GET and POST
+	@GetMapping("/list") // use this for only HTTP GET
 	public String listCustomers(Model theModel) {
 		
 		System.out.println("BRAD!!!!!! just inside CustomerController::listCustomers()");
