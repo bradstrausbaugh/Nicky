0abb54070461234c97b8839fcb6cbc37c4f72f25
brad <brad@brad-GN34>
2020-11-15 18:51:15 -0500

    Just updating a comment

--- spring-demo-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,7 +14,7 @@ public class MyDemoLoggingAspect {
 
 	// Before any call to a method that matches this signature... 
 	// later we'll see variations on these Pointcut Expressions
-	@Before("execution(public void addAccount())")
+	@Before("execution(public void addAccount())")  // any class here - more later
 	public void beforeAddAccountAdvice() {  // this can be named anything
 		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
 	}
